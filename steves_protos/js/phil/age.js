1e8eb8d6708772b44a0257be5d845fd3421f28c4
sportsfantasystats <sportsfantasystats@outlook.com>
2018-11-17 16:09:44 -0800

    Add files via upload

---
+++ steves_protos/js/phil/age.js
@@ -0,0 +1,235 @@
+
+var formatAsThousands = d3.format(",");
+var formatNumber = d3.format("d");
+var moving = false;
+
+ChartAge = function(_parentElement, _data){
+    this.parentElement = _parentElement;
+    this.data = _data; //data passed to this
+    this.displayData = []; //data filtered by year for display
+
+    // DEBUG RAW DATA
+    //console.log("ChartAge");
+    //console.log(this.data);
+
+    this.initVis();
+    //this.wrangleData("2017");
+}
+
+
+ChartAge.prototype.initVis = function(){
+    var vis = this;
+
+    vis.margin = { top: 40, right: 40, bottom: 40, left: 40 };
+
+    vis.width = 720 - vis.margin.left - vis.margin.right;
+    vis.height = 450 - vis.margin.top - vis.margin.bottom;
+
+    // SVG drawing bars
+    vis.svg = d3.select("#" + vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+    // Scales and axes
+    vis.x = d3.scaleBand()
+        .paddingInner(0.5)
+        .range([0, vis.width]);
+
+    vis.y = d3.scaleLinear()
+        .domain([0, 25000000])
+        .range([vis.height, 0]);
+
+    vis.xAxis = d3.axisBottom()
+        .scale(vis.x);
+
+    vis.yAxis = d3.axisLeft()
+        .scale(vis.y)
+        .tickFormat(d3.format(".2s"));
+
+    vis.svg.append("g")
+        .attr("class", "x-axis axis")
+        .attr("transform", "translate(0," + vis.height + ")");
+
+    vis.svg.append("g")
+        .attr("class", "y-axis axis")
+        .attr("transform", "translate(0,0)");
+
+    //initialize and attach slider
+    var yearRange = [];
+
+    for (var i = 0; i < vis.data.length; i++){
+        yearRange[i] = +vis.data[i].key;
+    }
+
+    //console.log("year range");
+    //console.log(yearRange);
+
+    //create slider
+    //Creation of the nouislider object.
+    vis.ageSlider = document.getElementById("ageSlider");
+
+    var minVal = d3.min(yearRange);
+    var maxVal = d3.max(yearRange);
+
+    noUiSlider.create(vis.ageSlider, {
+        start: [minVal],
+        range: {
+            'min': minVal,
+            'max': maxVal
+        },
+        step: 1,
+        connect: true,
+        pips: {
+            mode: 'steps',
+            stepped: true,
+            density:10
+        }
+    });
+
+    var currentVal = ageSlider.noUiSlider.get();
+    var timer;
+    var moving = false;
+    var duration = 400;
+
+    //bars
+    vis.bars = vis.svg.selectAll("rect");
+    var button = d3.select("#ageButton")
+        .on("click", function() {
+            //click triggers a loop through 2010 and 2017
+            var button = d3.select(this);
+            if (moving) {
+                clearInterval(timer);
+                button.text("Play");
+                moving = false;
+            } else {
+                button.text("Pause");
+                moving = true;
+                var i = ageSlider.noUiSlider.get();
+                timer = setInterval(function(){
+                    if (i <= maxVal){
+                        vis.ageSlider.noUiSlider.set(i);
+
+                        //debug
+                        console.log(i);
+
+                        i++;
+                        if (i === maxVal + 1) {
+                            i = minVal;
+                        }
+                    }
+                }, duration);
+            }
+
+            console.log("Clicked!");});
+
+    vis.ageSlider.noUiSlider.on('update', function (values, handle){
+        values[handle].innerHTML = values[handle];
+
+        //debug
+        //console.log(handle);
+        //console.log(values[handle]);
+
+        var stringYear = d3.format("")(values[handle]);
+        vis.wrangleData(stringYear);
+    });
+
+
+//    vis.wrangleData("2017");
+
+}
+
+ChartAge.prototype.wrangleData = function(myYear){
+    var vis = this;
+
+    //debug
+    //console.log(myYear);
+
+    vis.displayData = vis.data.filter(function(d){
+        return d.key == myYear;
+        })[0].values;
+
+    //debug
+    //console.log("Debug wrangleData");
+    //console.log(vis.displayData);
+
+    // Update the visualization
+    vis.updateVis();
+}
+
+ChartAge.prototype.updateVis = function(){
+    var vis = this;
+
+    //debug
+    //console.log("Entering updateVis");
+    //console.log(vis.displayData);
+
+    // Update x and y scale domains with updated data
+    var ageRanges = [];
+    for (var i = 0; i < vis.displayData.length; i++){
+        ageRanges[i] = vis.displayData[i].key;
+    }
+    //console.log(ageRanges);
+
+    vis.x.domain(ageRanges);
+
+    /*debug
+    console.log(d3.min(vis.displayData, function(d){
+        return d.values[0].Population;
+    }));
+    console.log(d3.max(vis.displayData, function(d){
+        return d.values[0].Population;
+    }));
+    */
+
+    // (2) Draw rectangles
+    var bars = vis.svg.selectAll("rect")
+        .data(vis.displayData);
+
+    bars.enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return vis.x(d.key);
+        })
+        .merge(bars)
+        .transition()
+        .duration(1000)
+        .attr("y", function(d) {
+            return vis.y(d.values[0].Population);
+        })
+        .attr("width", vis.x.bandwidth())
+        .attr("height", function(d) {
+            return vis.y(0) - vis.y(d.values[0].Population);
+        })
+
+        .attr("fill", "steelblue");
+
+    bars.on("mouseover", function(d) {
+            var tipAge, tipPop;
+
+            tipAge = "Age range: " + d.key;
+            tipPop = "Population: " + formatAsThousands(d.values[0].Population);
+
+            ageTooltip.transition()
+                .duration(200)
+                .style("opacity", .8);
+            ageTooltip.html("<br/>" + tipAge + "<br/>" + tipPop +  "<br/>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px");
+        })
+        .on("mouseout", function() {
+            ageTooltip.transition()
+                .duration(600)
+                .style("opacity", 0);
+        });
+
+    bars.exit().remove();
+
+    //console.log("updateVis done");
+
+    // Call axis functions with the new domain
+    vis.svg.select(".x-axis").call(vis.xAxis);
+    vis.svg.select(".y-axis").call(vis.yAxis);
+
+}
---
+++ steves_protos/js/steve/choropleth-crisis.js
@@ -0,0 +1,169 @@
+﻿var margin = { top: 10, right: 10, bottom: 10, left: 20 };
+
+var width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var svg2 = d3.select("#choropleth-crisis").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("fill", "steelblue");
+
+var lgnd = svg2.append("g");
+var txt = svg2.append("g"); 
+
+//Tooltip div element.
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+//Define projection
+var projection = d3.geoAlbersUsa()
+    .translate([width / 2, height / 2])
+    .scale([850]);
+
+var lsW = 20, lsH = 20;
+
+//Color scale with domain and range for population.
+var salesColorDomain = [200000, 250000, 300000, 400000, 500000];
+var salesColorRange = ['#f1eef6', '#d7b5d8', '#df65b0', '#dd1c77', '#980043']; 
+var salesLegendLabel = ["< 200K", "250K+", "300K+", "400K+", "500K+"];     
+
+var salesColor = d3.scaleThreshold()
+    .domain(salesColorDomain)
+    .range(salesColorRange); 
+
+//Define path generator, using the Albers USA projection
+var path = d3.geoPath()
+    .projection(projection);
+
+queue()
+    .defer(d3.csv, "data/current_sales_by_state_v2.csv")
+    .defer(d3.json, "https://d3datafiles.blob.core.windows.net/coursedata/d3finalproject/us-states.json")
+    .defer(d3.csv, "data/top_25_us_cities_for_sales.csv")
+    .await(function(error, data, json, topcities) {
+
+        if (error) throw error;
+
+        //Merge the ag. data and GeoJSON
+        //Loop through once for each ag. data value
+        for (var i = 0; i < data.length; i++) {
+
+            //Grab state name
+            var dataState = data[i].state;
+
+            //Grab data value, and convert from string to float
+            var dataValue = parseFloat(data[i].value);
+
+            //Find the corresponding state inside the GeoJSON
+            for (var j = 0; j < json.features.length; j++) {
+
+                var jsonState = json.features[j].properties.name;
+
+                if (dataState == jsonState) {
+
+                    //Copy the data value into the JSON
+                    json.features[j].properties.value = dataValue;
+
+                    //Stop looking through the JSON
+                    break;
+
+                }
+            }
+        }
+
+        //console.log(json); 
+
+        //Bind data and create one path per GeoJSON feature
+        svg2.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", function (d) {
+                //Get data value
+                var value = d.properties.value;
+
+                if (value) {
+                    //If value exists…
+                    return salesColor(value);
+                } else {
+                    //If value is undefined…
+                    return "#ccc";
+                }
+            })
+            .on("mouseover", function (d) {
+                d3.select(this).transition().duration(300).style("opacity", 1);
+                div.transition()
+                    .duration(200)
+                    .style("opacity", 1);
+                div.style("display", "inline");
+                div.html("<div><span><p><strong>State: </strong>" + d.properties.name + "</p>" +
+                    "<p><strong>AVG Value: </strong>" + "$" + d.properties.value + "</p>" +
+                        "</span></div>")
+                    .style("left", (d3.event.pageX + 10) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout",
+            function () {
+                d3.select(this)
+                    .transition().duration(300)
+                    .style("opacity", 0.8);
+                div.transition().duration(300)
+                    .style("opacity", 0);
+            });
+
+        svg2.selectAll("circle")
+            .data(topcities)
+            .enter()
+            .append("circle")
+            .attr("cx",
+                function(d) {
+                    return projection([d.Long, d.Lat])[0];
+                })
+            .attr("cy",
+                function(d) {
+                    return projection([d.Long, d.Lat])[1];
+                })
+            .attr("r",
+                function(d) {
+                    return Math.sqrt(parseInt(d.Sales) * 0.0004);
+                })
+            .style("fill", "black")
+            .style("stroke", "white")
+            .style("stroke-width", 0.25)
+            .style("opacity", 0.4)
+            .append("title")
+            .text(function(d) {
+                return d.City + ": " + "$" + d.Sales; 
+            }); 
+
+        //Draw the legend for the population choropleth.
+
+        var legend = lgnd.selectAll("rect")
+            .data(salesColorDomain);
+
+        legend.enter().append("rect")
+            .merge(legend)
+            .attr("class", "legend")
+            .attr("x", 20)
+            .attr("y", function (d, i) { return height - (i * lsW) - 2 * lsH; })
+            .attr("width", lsW)
+            .attr("height", lsH)
+            .style("fill", function (d, i) { return salesColor(d); })
+            .style("opacity", 0.8)
+            .attr("transform", "translate(650, 40)");
+
+        legend.exit().remove();
+
+        var legendLabels = txt.selectAll("text")
+            .data(salesLegendLabel);
+
+        legendLabels.enter().append("text")
+            .merge(legendLabels)
+            .attr("x", 50)
+            .attr("y", function (d, i) { return height - (i * lsH) - lsH - 4; })
+            .text(function (d, i) { return salesLegendLabel[i]; })
+            .attr("transform", "translate(650,40)");
+
+        legendLabels.exit().remove(); 
+    });
